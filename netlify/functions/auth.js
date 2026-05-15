0d585dea896451ffd60c20c6f016efbcadac7eb2
Romain-14 <romain14f@msn.com>
2026-05-15 19:14:51 +0200

    fix: decap, github, app auth

---
+++ netlify/functions/auth.js
@@ -0,0 +1,22 @@
+export async function handler(event) {
+  const code = event.queryStringParameters.code;
+
+  const res = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+    body: new URLSearchParams({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    }),
+  });
+
+  const data = await res.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+}
